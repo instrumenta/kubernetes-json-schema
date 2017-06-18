ae92552452ef9ab30958159294df92d35f813850
Gareth Rushgrove <gareth@morethanseven.net>
2017-06-18 17:03:29 +0100

    Initial commit of Kubernetes schemas for 1.5.0 until 1.6.6
    This is an arbitrary range, so if anyone wants older versions then let
    me know and I'll generate. Once the tools I'm using to extract the
    schemas are open source then anyone can regenerate and send PRs.

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -xe
+
+# This script uses openapi2jsonschema to generate a set of JSON schemas for
+# the specified Kubernetes versions in three different flavours:
+#
+#   X.Y.Z - URL referenced based on the specified GitHub repository
+#   X.Y.Z-standalone - de-referenced schemas, more useful as standalone documents
+#   X.Y.Z-local - relative references, useful to avoid the network dependency
+
+REPO="garethr/kubernetes=json-schema"
+
+declare -a arr=(1.6.6
+                1.6.5
+                1.6.4
+                1.6.3
+                1.6.2
+                1.6.1
+                1.6.0
+                1.5.6
+                1.5.4
+                1.5.3
+                1.5.2
+                1.5.1
+                1.5.0
+                )
+
+for version in "${arr[@]}"
+do
+    schema=https://raw.githubusercontent.com/kubernetes/kubernetes/v${version}/api/openapi-spec/swagger.json
+    prefix=https://raw.githubusercontent.com/${REPO}/master/v${version}/_definitions.json
+
+    python manage.py -o "${version}-standalone" -s "${schema}" --stand-alone
+    python manage.py -o "${version}-local" -s "${schema}"
+    python manage.py -o "${version}" -s "${schema}" --prefix "${prefix}"
+done
