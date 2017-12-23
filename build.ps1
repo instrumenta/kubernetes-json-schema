3ab60fee99effde698f84d18c6f46e9547f4a47a
Gareth Rushgrove <gareth@morethanseven.net>
2017-12-23 16:36:25 +0000

    updated with latest k8s versions

--- build.ps1
+++ build.ps1
@@ -9,12 +9,30 @@
 $repo="garethr/kubernetes-json-schema"
 
 $arr = @("master",
+         "v1.9.0",
+         "v1.8.5",
+         "v1.8.4",
+         "v1.8.3",
+         "v1.8.2",
+         "v1.8.1",
+         "v1.8.0",
+         "v1.7.11",
+         "v1.7.10",
+         "v1.7.9",
+         "v1.7.8",
+         "v1.7.7",
+         "v1.7.6",
          "v1.7.5",
          "v1.7.4",
          "v1.7.3",
          "v1.7.2",
          "v1.7.1",
          "v1.7.0",
+         "v1.6.13",
+         "v1.6.12",
+         "v1.6.11",
+         "v1.6.10",
+         "v1.6.9",
          "v1.6.8",
          "v1.6.7",
          "v1.6.6",
@@ -37,10 +55,10 @@ foreach($version in $arr) {
     $schema="https://raw.githubusercontent.com/kubernetes/kubernetes/${version}/api/openapi-spec/swagger.json"
     $prefix="https://raw.githubusercontent.com/${repo}/master/${version}/_definitions.json"
 
-    docker run --rm -v ${PWD}:/out garethr/openapi2jsonschema -o "$version-standalone-strict" --kubernetes --stand-alone --strict "$schema"
-    docker run --rm -v ${PWD}:/out garethr/openapi2jsonschema -o "$version-standalone" --kubernetes --stand-alone "$schema"
-    docker run --rm -v ${PWD}:/out garethr/openapi2jsonschema -o "$version-local" --kubernetes "$schema"
-    docker run --rm -v ${PWD}:/out garethr/openapi2jsonschema -o "$version" --kubernetes --prefix "$prefix" "$schema"
+    #docker run --rm -v ${PWD}:/out garethr/openapi2jsonschema -o "$version-standalone-strict" --kubernetes --stand-alone --strict "$schema"
+    #docker run --rm -v ${PWD}:/out garethr/openapi2jsonschema -o "$version-standalone" --kubernetes --stand-alone "$schema"
+    #docker run --rm -v ${PWD}:/out garethr/openapi2jsonschema -o "$version-local" --kubernetes "$schema"
+    #docker run --rm -v ${PWD}:/out garethr/openapi2jsonschema -o "$version" --kubernetes --prefix "$prefix" "$schema"
     dos2unix.exe "$version-standalone-strict/*"
     dos2unix.exe "$version-standalone/*"
     dos2unix.exe "$version-local/*"
