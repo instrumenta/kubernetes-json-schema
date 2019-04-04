935e1986b953a1b28ff2ffd302cd705be0701766
Gareth Rushgrove <gareth@morethanseven.net>
2019-04-04 18:09:53 +0100

    changed the domain for finding subschemas

--- build.sh
+++ build.sh
@@ -7,8 +7,6 @@
 #   X.Y.Z-standalone - de-referenced schemas, more useful as standalone documents
 #   X.Y.Z-local - relative references, useful to avoid the network dependency
 
-REPO="garethr/kubernetes-json-schema"
-
 declare -a arr=(
     #master
     v1.14.0
@@ -116,15 +114,14 @@ declare -a old=(
 for version in "${arr[@]}"
 do
 schema=https://raw.githubusercontent.com/kubernetes/kubernetes/${version}/api/openapi-spec/swagger.json
-prefix=https://raw.githubusercontent.com/${REPO}/master/${version}/_definitions.json
+prefix=https://kubernetesjsonschema.dev/${version}/_definitions.json
 
 openapi2jsonschema -o "${version}-standalone-strict" --expanded --kubernetes --stand-alone --strict "${schema}"
 openapi2jsonschema -o "${version}-standalone" --expanded --kubernetes --stand-alone "${schema}"
 openapi2jsonschema -o "${version}-local" --expanded --kubernetes "${schema}"
 openapi2jsonschema -o "${version}" --expanded --kubernetes --prefix "${prefix}" "${schema}"
-
-#openapi2jsonschema -o "${version}-standalone-strict" --kubernetes --stand-alone --strict "${schema}"
-#openapi2jsonschema -o "${version}-standalone" --kubernetes --stand-alone "${schema}"
-#openapi2jsonschema -o "${version}-local" --kubernetes "${schema}"
-#openapi2jsonschema -o "${version}" --kubernetes --prefix "${prefix}" "${schema}"
+openapi2jsonschema -o "${version}-standalone-strict" --kubernetes --stand-alone --strict "${schema}"
+openapi2jsonschema -o "${version}-standalone" --kubernetes --stand-alone "${schema}"
+openapi2jsonschema -o "${version}-local" --kubernetes "${schema}"
+openapi2jsonschema -o "${version}" --kubernetes --prefix "${prefix}" "${schema}"
 done
